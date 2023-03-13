ebd1a9c0f74ae713adb4146da09507da9a22fccb
Mo7amedW2geh <63908554+Mo7amedW2geh@users.noreply.github.com>
2023-03-13 16:39:27 +0200

    Create sinx.cpp

---
+++ Flow charts/sinx.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    double x;
+
+    std::cout << "enter x: ";
+    std::cin >> x;
+
+    int n = 2;
+    double t = x;
+    double sinx = 0;
+
+    while(std::fabs(t) > std::pow(10, -6)) {
+        sinx = sinx + t;
+        t = t * (-std::pow(x, 2)) / (n * (n + 1));
+        n = n + 2;
+    }
+
+    std::cout << "sqrt(sin(" << x << ")) = " << std::sqrt(sinx) << std::endl;
+
+    return 0;
+}
