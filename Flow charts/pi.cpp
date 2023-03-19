91f99f23e7766b6777a0753f4fee6d7ac56bb8d9
Mo7amedW2geh <63908554+Mo7amedW2geh@users.noreply.github.com>
2023-03-19 18:59:45 +0200

    Update pi.cpp

--- Flow charts/pi.cpp
+++ Flow charts/pi.cpp
@@ -8,12 +8,14 @@ int main() {
     float pi = 0;
 
     while(std::fabs(t) > std::pow(10, -4)) {
+        //t = (4 * std::pow(-1, n)) / (2*n + 1);
         t = (std::pow(-1, n)) / (2*n + 1);
         pi = pi + t;
         n = n + 1;
     }
     
-    // instead of t = (4 * std::pow(-1, n)) / (2*n + 1);
+    // instead "of t = (4 * std::pow(-1, n)) / (2*n + 1);"
+    // we add "pi = pi * 4"
     pi = pi * 4
      
         
