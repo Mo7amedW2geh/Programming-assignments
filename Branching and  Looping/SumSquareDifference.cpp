345428d35bddf62807ef61033e7d0671d688cdf4
Mo7amedW2geh <63908554+Mo7amedW2geh@users.noreply.github.com>
2023-04-27 19:04:40 +0200

    Update SumSquareDifference.cpp

--- Branching and  Looping/SumSquareDifference.cpp
+++ Branching and  Looping/SumSquareDifference.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
+	
+	//Problem 6 (Project Euler)
+	//The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+	
+	
 	int n = 100;     //we can use cin
 
 	int sum = 0;     //The sum of the squares
