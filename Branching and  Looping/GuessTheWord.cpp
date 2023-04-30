5c93983b273446ea3aa88ae2bd8fc9b9b1c32075
Mo7amedW2geh <63908554+Mo7amedW2geh@users.noreply.github.com>
2023-04-30 15:54:00 +0200

    Update GuessTheWord.cpp

--- Branching and  Looping/GuessTheWord.cpp
+++ Branching and  Looping/GuessTheWord.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <cctype>
 
 using namespace std;
 
@@ -14,18 +15,27 @@ string getRandomWord(string wordList[], int size) {
 
 // Prompts the user to choose a theme and returns a word from the corresponding word list
 string getTheme() {
-    string themes[] = {"fruits", "animals", "countries", "programming languages" };
+    string themes[] = {"Fruits", "Animals", "Countries", "Programming Languages"};
 
-    cout << "Choose a theme: " << endl;
+    cout << "Choose a theme (1-4): " << endl;
     for (int i = 0; i < 4; i++) {
         cout << i + 1 << ". " << themes[i] << endl;
     }
+    cout << ">";
 
     int themeChoice;
     cin >> themeChoice;
 
+    // input validation
+    while (cin.fail() || themeChoice < 1 || themeChoice > 4) {
+        cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cin >> themeChoice;
+    }
+
     string wordList[8];
-    int wordListSize;
+    int wordListSize = 0;
 
     if (themeChoice == 1) {
         string words[] = {"apple", "banana", "cherry", "kiwi", "lemon", "orange", "strawberry", "watermelon"};
@@ -55,25 +65,62 @@ string getTheme() {
             wordList[i] = words[i];
         }
     }
-    else {
-        cout << "Invalid choice. Please try again." << endl;
-        return getTheme();
-    }
-
-    cout << "\n\nThe theme is \"" << themes[themeChoice - 1] << "\"\n\n";
 
     return getRandomWord(wordList, wordListSize);
 }
 
-int main() 
+int getDifficulty() {
+
+    const int easyMaxGuesses = 8;
+    const int mediumMaxGuesses = 5;
+    const int hardMaxGuesses = 3;
+    const int insaneMaxGuesses = 1;
+
+    string difficulty[] = {"Easy", "Medium", "Hard", "Insane"};
+
+    cout << "Choose a difficulty (1-4): " << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << i + 1 << ". " << difficulty[i] << endl;
+    }
+    cout << ">";
+
+    int difficultyChoice;
+    cin >> difficultyChoice;
+
+    // input validation
+    while (cin.fail() || difficultyChoice < 1 || difficultyChoice > 4) {
+        cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cin >> difficultyChoice;
+    }
+
+    if (difficultyChoice == 1) {
+        return easyMaxGuesses;
+    }
+    else if (difficultyChoice == 2) {
+        return mediumMaxGuesses;
+    }
+    else if (difficultyChoice == 3) {
+        return hardMaxGuesses;
+    }
+    else if (difficultyChoice == 4) {
+        return insaneMaxGuesses;
+    }
+
+}
+
+int main()
 {
 
     cout << "Welcome to The Game!\n\n";
 
     string wordToGuess = getTheme();
     int wordLength = wordToGuess.length();
-    int maxGuesses = 8;
+
+    int maxGuesses = getDifficulty();
     int remainingGuesses = maxGuesses;
+
     string lettersGuessed = "";
     string currentWordState = string(wordLength, '_');
 
@@ -84,21 +131,27 @@ int main()
         cout << "Guessed letters: " << lettersGuessed << endl;
         cout << "\nGuess a letter: ";
 
-        char guess;
-        cin >> guess;
+        string Guess;
+        cin >> Guess;
+
+        // Check if the input is a single letter
+        if (Guess.length() != 1) {
+            cout << "\n\nInvalid input. Please enter a single letter." << endl;
+            continue;
+        }
 
         // Check if the input is a valid letter
-        if (!isalpha(guess)) {
-            cout << "\nInvalid input. Please enter a letter." << endl;
+        if (!isalpha(Guess[0])) {
+            cout << "\n\nInvalid input. Please enter a valid letter." << endl;
             continue;
         }
 
         // convert guess to lowercase
-        guess = tolower(guess);
+        char guess = tolower(Guess[0]);
 
         // check if the guess has already been made
         if (lettersGuessed.find(guess) != string::npos) {
-            cout << "\nYou've already guessed that letter. Please try again." << endl;
+            cout << "\n\nYou've already guessed that letter. Please try again." << endl;
             continue;
         }
 
@@ -107,7 +160,7 @@ int main()
 
         // check if the guess is in the word
         if (wordToGuess.find(guess) != string::npos) {
-            cout << "\nGood guess! That letter is in the word." << endl;
+            cout << "\n\nGood guess! That letter is in the word." << endl;
             for (int i = 0; i < wordLength; i++) {
                 if (wordToGuess[i] == guess) {
                     currentWordState[i] = guess;
@@ -115,19 +168,19 @@ int main()
             }
         }
         else {
-            cout << "\nSorry, the letter is not in the word." << endl;
+            cout << "\n\nSorry, the letter is not in the word." << endl;
             remainingGuesses--;
         }
     }
 
-        // print the result of the game
-        if (currentWordState == wordToGuess) {
-            cout << "\nCongratulations! You guessed the word \"" << wordToGuess << "\"." << endl;
-        }
-        else {
-            cout << "\nSorry, you ran out of guesses. The word was \"" << wordToGuess << "\"." << endl;
-        }
+    // print the result of the game
+    if (currentWordState == wordToGuess) {
+        cout << "\nCongratulations! You guessed the word \"" << wordToGuess << "\"." << endl;
+    }
+    else {
+        cout << "\nSorry, you ran out of guesses. The word was \"" << wordToGuess << "\"." << endl;
+    }
 
-        return 0;
-    
+    return 0;
+
 }
